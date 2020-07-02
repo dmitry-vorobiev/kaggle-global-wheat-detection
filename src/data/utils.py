af76cecd2a3c2911c2ee5ae0587f7b3402b79db7
dmitry-vorobiev <25718561+dmitry-vorobiev@users.noreply.github.com>
2020-07-02 20:01:50 +0300

    copy train script from dmitry-vorobiev/hair_seg

--- src/data/utils.py
+++ src/data/utils.py
@@ -1,11 +1,11 @@
-import torch
 from torch import Tensor
+# noinspection PyUnresolvedReferences
+from torch.utils.data.dataloader import default_collate
 from typing import Any, Iterable, List, Tuple
 
 
 def collate(batch: Iterable[Tuple[Any, Any]]) -> Tuple[Tensor, List[Tensor]]:
     images, targets = tuple(zip(*batch))
-    secret_sauce = torch.utils.data._utils.collate.default_collate
-    images = secret_sauce(images)
-    targets = list(map(secret_sauce, targets))
+    images = default_collate(images)
+    targets = list(map(default_collate, targets))
     return images, targets
