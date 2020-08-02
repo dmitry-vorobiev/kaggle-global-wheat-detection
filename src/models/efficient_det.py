ea9d7d3384d8bc60fb200999bf0f4586883f00b7
dmitry-vorobiev <25718561+dmitry-vorobiev@users.noreply.github.com>
2020-08-02 23:29:51 +0300

    add cluster_nms to effdet train bench

--- src/models/efficient_det.py
+++ src/models/efficient_det.py
@@ -18,11 +18,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import copy
-
-from effdet import DetBenchTrain, DetBenchPredict, EfficientDet, load_checkpoint, load_pretrained
+import torch
+import torch.nn as nn
+from effdet import DetBenchPredict, EfficientDet, load_checkpoint, load_pretrained
+from effdet.anchors import Anchors, AnchorLabeler, MAX_DETECTIONS_PER_IMAGE, MAX_DETECTION_POINTS, \
+    decode_box_outputs, clip_boxes_xyxy
+from effdet.bench import _post_process, _batch_detection
 from effdet.efficientdet import HeadNet
 
 from loss.efficient_det import DetectionLoss
+from nms.cluster_nms import cluster_nms_dist_iou
 
 
 def create_model_from_config(config, bench_name='', pretrained=False, checkpoint_path='', **kwargs):
@@ -47,3 +52,118 @@ def create_model_from_config(config, bench_name='', pretrained=False, checkpoint
     elif bench_name == 'predict':
         model = DetBenchPredict(model, config)
     return model
+
+
+def generate_detections_cluster_nms(
+        cls_outputs, box_outputs, anchor_boxes, indices, img_scale, img_size, min_score,
+        iou_threshold, max_det_per_image: int = MAX_DETECTIONS_PER_IMAGE):
+    """Generates detections with RetinaNet model outputs and anchors.
+
+    Args:
+        cls_outputs: a torch tensor with shape [N, 1], which has the highest class
+            scores on all feature levels. The N is the number of selected
+            top-K total anchors on all levels.  (k being MAX_DETECTION_POINTS)
+
+        box_outputs: a torch tensor with shape [N, 4], which stacks box regression
+            outputs on all feature levels. The N is the number of selected top-k
+            total anchors on all levels. (k being MAX_DETECTION_POINTS)
+
+        anchor_boxes: a torch tensor with shape [N, 4], which stacks anchors on all
+            feature levels. The N is the number of selected top-k total anchors on all levels.
+
+        indices: a torch tensor with shape [N], which is the indices from top-k selection.
+
+        classes: a torch tensor with shape [N], which represents the class
+            prediction on all selected anchors from top-k selection.
+
+        img_scale: a float tensor representing the scale between original image
+            and input image for the detector. It is used to rescale detections for
+            evaluating with the original groundtruth annotations.
+
+        max_det_per_image: an int constant, added as argument to make torchscript happy
+
+    Returns:
+        detections: detection results in a tensor with shape [MAX_DETECTION_POINTS, 6],
+            each row representing [x, y, width, height, score, class]
+    """
+    anchor_boxes = anchor_boxes[indices, :]
+
+    # apply bounding box regression to anchors
+    boxes = decode_box_outputs(box_outputs.float(), anchor_boxes, output_xyxy=True)
+    boxes = clip_boxes_xyxy(boxes, img_size / img_scale)  # clip before NMS better?
+
+    scores = cls_outputs.sigmoid().float()
+    confidence, _ = torch.max(scores, dim=-1)
+    keep = confidence >= min_score
+    scores = scores[keep]
+    boxes = boxes[keep, :]
+
+    boxes, scores, classes = cluster_nms_dist_iou(
+        boxes, scores, iou_threshold=iou_threshold, top_k=MAX_DETECTION_POINTS)
+    boxes = boxes[:max_det_per_image]
+    scores = scores[:max_det_per_image, None]
+    classes = classes[:max_det_per_image, None]
+
+    # xyxy to xywh & rescale to original image
+    boxes[:, 2] -= boxes[:, 0]
+    boxes[:, 3] -= boxes[:, 1]
+    boxes *= img_scale
+
+    classes += 1  # back to class idx with background class = 0
+
+    # stack em and pad out to MAX_DETECTIONS_PER_IMAGE if necessary
+    detections = torch.cat([boxes, scores, classes.float()], dim=1)
+    num_pad = max_det_per_image - len(detections)
+    if num_pad > 0:
+        detections = torch.cat([
+            detections,
+            torch.zeros((num_pad, 6), device=detections.device, dtype=detections.dtype)
+        ], dim=0)
+    return detections
+
+
+def _batch_detection_cluster_nms(batch_size: int, class_out, box_out, anchor_boxes, indices,
+                                 img_scale, img_size, min_score, iou_threshold):
+    batch_detections = []
+    # FIXME we may be able to do this as a batch with some tensor reshaping/indexing, PR welcome
+    for i in range(batch_size):
+        detections = generate_detections_cluster_nms(
+            class_out[i], box_out[i], anchor_boxes, indices[i], img_scale[i], img_size[i],
+            min_score, iou_threshold)
+        batch_detections.append(detections)
+    return torch.stack(batch_detections, dim=0)
+
+
+class DetBenchTrain(nn.Module):
+    def __init__(self, model, config):
+        super(DetBenchTrain, self).__init__()
+        self.config = config
+        self.model = model
+        self.anchors = Anchors(
+            config.min_level, config.max_level,
+            config.num_scales, config.aspect_ratios,
+            config.anchor_scale, config.image_size)
+        self.anchor_labeler = AnchorLabeler(self.anchors, config.num_classes, match_threshold=0.5)
+        self.loss_fn = DetectionLoss(self.config)
+
+    def forward(self, x, target):
+        class_out, box_out = self.model(x)
+        cls_targets, box_targets, num_positives = self.anchor_labeler.batch_label_anchors(
+            x.shape[0], target['bbox'], target['cls'])
+        loss, class_loss, box_loss = self.loss_fn(class_out, box_out, cls_targets, box_targets, num_positives)
+        output = dict(loss=loss, class_loss=class_loss, box_loss=box_loss)
+        if not self.training:
+            # if eval mode, output detections for evaluation
+            class_out, box_out, indices, classes = _post_process(self.config, class_out, box_out)
+            if self.config.custom_nms:
+                min_score = self.config.nms_min_score
+                iou_threshold = self.config.nms_max_iou
+                detections = _batch_detection_cluster_nms(
+                    x.shape[0], class_out, box_out, self.anchors.boxes, indices,
+                    target['img_scale'], target['img_size'], min_score, iou_threshold)
+            else:
+                detections = _batch_detection(
+                    x.shape[0], class_out, box_out, self.anchors.boxes, indices, classes,
+                    target['img_scale'], target['img_size'])
+            output['detections'] = detections
+        return output
--- src/nms/cluster_nms.py
+++ src/nms/cluster_nms.py
@@ -1,8 +1,11 @@
 import torch
 from torch import Tensor
 
+EPS = torch.tensor(1e-8)
 
-def diou_ab(box_a: Tensor, box_b: Tensor, gamma=0.9, eps=1e-8):
+
+@torch.jit.script
+def dist_iou_ab(box_a: Tensor, box_b: Tensor, eps=EPS):
     """
     Args:
         box_a: tensor of shape [batch_size, boxes_a, 4]
@@ -14,8 +17,8 @@ def diou_ab(box_a: Tensor, box_b: Tensor, gamma=0.9, eps=1e-8):
     https://github.com/Zzh-tju/CIoU/blob/8995056b1e93b86d03c384f042514391b70e58e0/layers/functions/detection.py#L162
     https://github.com/Zzh-tju/CIoU/blob/8995056b1e93b86d03c384f042514391b70e58e0/layers/box_utils.py#L82
     """
-    assert box_a.ndim == 3
-    assert box_b.ndim == 3
+    assert box_a.dim() == 3
+    assert box_b.dim() == 3
     assert box_a.size(0) == box_b.size(0)
 
     A, B = box_a.size(1), box_b.size(1)
@@ -27,7 +30,7 @@ def diou_ab(box_a: Tensor, box_b: Tensor, gamma=0.9, eps=1e-8):
 
     inter_hw = torch.clamp_min_(inter_yx1 - inter_yx0, 0)
     inter_area = torch.prod(inter_hw, dim=-1)
-    del inter_hw, inter_yx0, inter_yx1
+    # del inter_hw, inter_yx0, inter_yx1
 
     hw_a = box_a[..., 2:4] - box_a[..., :2]
     hw_b = box_b[..., 2:4] - box_b[..., :2]
@@ -37,7 +40,7 @@ def diou_ab(box_a: Tensor, box_b: Tensor, gamma=0.9, eps=1e-8):
 
     union_area = area_a + area_b - inter_area
     iou = inter_area / (union_area + eps)
-    del inter_area, union_area, area_a, area_b, hw_a, hw_b
+    # del inter_area, union_area, area_a, area_b, hw_a, hw_b
 
     center_a = (box_a[..., :2] + box_a[..., 2:4]) / 2
     center_b = (box_b[..., :2] + box_b[..., 2:4]) / 2
@@ -47,35 +50,37 @@ def diou_ab(box_a: Tensor, box_b: Tensor, gamma=0.9, eps=1e-8):
     clos_yx1 = torch.max(box_a[..., 2:4], box_b[..., 2:4])
     clos_hw = torch.clamp_min_(clos_yx1 - clos_yx0, 0)
     clos_diag = torch.pow(clos_hw, 2).sum(dim=-1)
-    del clos_yx0, clos_yx1, clos_hw, center_a, center_b
+    # del clos_yx0, clos_yx1, clos_hw, center_a, center_b
 
     dist = inter_diag / (clos_diag + eps)
-    return iou - dist ** gamma
+    return iou - dist ** 0.95
 
 
-def cluster_nms_diou(boxes, scores, thresh=0.5, top_k=200):
-    assert boxes.ndim == 2
-    assert boxes.size(-1) == 4
+def cluster_nms_dist_iou(boxes: Tensor, scores: Tensor, iou_threshold=0.5, top_k=200):
+    assert boxes.dim() == 2
+    assert scores.dim() == 2
+    assert boxes.size(0) == scores.size(0)
 
-    scores, classes = torch.max(scores, dim=0)
+    scores, classes = torch.max(scores, dim=1)
     # scores: [detections]
-    _, idx = scores.sort(0, descending=True)
+    _, idx = scores.sort(descending=True)
     idx = idx[:top_k]
+    # add batch dim
     top_k_boxes = boxes[idx][None, ...]
 
     # [1, top_k, top_k] -> [top_k, top_k]
-    iou = diou_ab(top_k_boxes, top_k_boxes)[0]
+    iou = dist_iou_ab(top_k_boxes, top_k_boxes)[0]
     iou = iou.triu_(diagonal=1)
-    best_iou = torch.zeros(top_k)
+    best_iou = torch.zeros_like(idx)
 
     for i in range(top_k):
         iou0 = iou
         best_iou, _ = torch.max(iou, dim=0)
         # keep far away boxes
-        keep = (best_iou <= thresh)[:, None].expand_as(iou)
+        keep = (best_iou <= iou_threshold)[:, None].expand_as(iou)
         iou = torch.where(keep, iou, torch.zeros_like(iou))
         if (iou == iou0).all():
             break
 
-    idx = idx[best_iou <= thresh]
+    idx = idx[best_iou <= iou_threshold]
     return boxes[idx], scores[idx], classes[idx]
